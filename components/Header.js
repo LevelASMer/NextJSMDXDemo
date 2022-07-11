ded7fcd6a7711a22f35fbd31c00d01d64e792f78
LevelASMer <zx132435@naver.com>
2022-07-11 19:33:13 +0900

    add link

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,24 @@
+import Link from 'next/link';
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <>
+      <h1 className="title">
+        <Link href="/">
+          <a>{title}</a>
+        </Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/upcoming">
+              <a>Upcoming</a>
+            </Link>
+          </li>
+          <li>Resources</li>
+          <li>FAQ</li>
+        </ul>
+      </nav>
+    </>
+  );
 }
---
+++ pages/upcoming.js
@@ -0,0 +1,9 @@
+import Upcoming from '../mdx/upcoming.mdx';
+
+export default function Banner(props) {
+  return (
+    <>
+      <Upcoming />
+    </>
+  );
+}
